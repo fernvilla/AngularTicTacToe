5f1a540b4ef1aa36d8d142e444d68678eaacb98a
fernvilla <fvilla18@gmail.com>
2014-01-15 21:44:09 -0800

    apply css styling to board

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 function TicTacToeCtrl ($scope) {
+	var turn = 0;
 
 	$scope.cells = ['','','','','','','','',''];
 
 	$scope.reset = function() {
 		$scope.cells = ['','','','','','','','',''];
+		turn = 0;
 	};
 
 	$scope.nextMove = function($index) {
-		$scope.cells[$index] = 'X';
+		var move = $scope.cells[$index];
+		if (turn % 2==0 && move==''){
+			$scope.cells[$index] = 'X';
+			turn++;
+		}
+		else if (turn % 2 == 1 && move==''){
+			$scope.cells[$index] = 'O';
+			turn++;
+		}
 	};
+
+	$scope.checkWin = function($index) {
+	
+	}
 
 };
