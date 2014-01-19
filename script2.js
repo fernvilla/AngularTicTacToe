a44af1f491a6302ae5e687069f78fe7631e197d8
fernvilla <fvilla18@gmail.com>
2014-01-19 14:40:00 -0800

    restart refactored code

--- script2.js
+++ script2.js
@@ -1,22 +1,88 @@
 function TicTacToeCtrl ($scope) {
-	$scope.setBoard = function() {
-		//Initialize array
+	$scope.cells = ['','','','','','','','',''];
+	$scope.turn = 0;
+	$scope.play = true;
+	$scope.p1score = 0;
+	$scope.p2score = 0;
+	$scope.ties = 0;
+	$scope.nextPlayer = $scope.player1 + ' is next!';
+	$scope.stylePath = 'style.css';
+
+	$scope.nextMove = function(x) {
+		if ($scope.play) {	
+			if ($scope.turn % 2 == 0 && $scope.cells[x] == '') {
+				$scope.cells[x] = 'X';
+				$scope.turn++;
+				$scope.nextPlayer = $scope.player2 + ' is next!';
+			}
+			else if ($scope.cells[x] == '') {
+				$scope.cells[x] = 'O';
+				$scope.turn++;
+				$scope.nextPlayer = $scope.player1 + ' is next!';
+			}
+			$scope.checkWin();
+		};
+	};
+
+	$scope.checkWin = function() {
+		if (($scope.cells[0] =='X' && $scope.cells[1]=='X' && $scope.cells[2] =='X') ||
+			($scope.cells[3] =='X' && $scope.cells[4]=='X' && $scope.cells[5] =='X') ||
+			($scope.cells[6] =='X' && $scope.cells[7]=='X' && $scope.cells[8] =='X') ||
+			($scope.cells[0] =='X' && $scope.cells[3]=='X' && $scope.cells[6] =='X') ||
+			($scope.cells[1] =='X' && $scope.cells[4]=='X' && $scope.cells[7] =='X') ||
+			($scope.cells[2] =='X' && $scope.cells[5]=='X' && $scope.cells[8] =='X') ||
+			($scope.cells[0] =='X' && $scope.cells[4]=='X' && $scope.cells[8] =='X') ||
+			($scope.cells[2] =='X' && $scope.cells[4]=='X' && $scope.cells[6] =='X'))
+		{
+			$scope.winner = $scope.player1 + ' wins in ' + $scope.turn + ' moves!';
+			$scope.nextPlayer = '';
+			$scope.p1score++;
+			$scope.play = false;
+		}
+		else if (($scope.cells[0] =='O' && $scope.cells[1]=='O' && $scope.cells[2] =='O') ||
+			($scope.cells[3] =='O' && $scope.cells[4]=='O' && $scope.cells[5] =='O') ||
+			($scope.cells[6] =='O' && $scope.cells[7]=='O' && $scope.cells[8] =='O') ||
+			($scope.cells[0] =='O' && $scope.cells[3]=='O' && $scope.cells[6] =='O') ||
+			($scope.cells[1] =='O' && $scope.cells[4]=='O' && $scope.cells[7] =='O') ||
+			($scope.cells[2] =='O' && $scope.cells[5]=='O' && $scope.cells[8] =='O') ||
+			($scope.cells[0] =='O' && $scope.cells[4]=='O' && $scope.cells[8] =='O') ||
+			($scope.cells[2] =='O' && $scope.cells[4]=='O' && $scope.cells[6] =='O'))
+		{
+			$scope.winner = $scope.player2 +' wins in ' + $scope.turn + ' moves!';
+			$scope.nextPlayer = '';
+			$scope.p2score++;
+			$scope.play = false;
+		}
+		else if ($scope.turn == 9) {
+			$scope.winner = 'Draw!';
+			$scope.nextPlayer = '';
+			$scope.ties++;
+			$scope.play = false;
+		}
+	};
+
+	$scope.resetBoard = function() {
 		$scope.cells = ['','','','','','','','',''];
-		//Set first player string and score totals to zero
-		$scope.nextPlayer = 'X';
+		$scope.turn = 0;
+		$scope.play = true;
+		$scope.nextPlayer = $scope.player1 + ' is next!';
+		$scope.winner = '';
+	};
+
+	$scope.clearTotals = function() {
 		$scope.p1score = 0;
 		$scope.p2score = 0;
 		$scope.ties = 0;
-		//Set initial stylesheet
-		$scope.stylePath = 'style.css';
+		$scope.player1 = '';
+		$scope.player2 = '';
+	};
+
+	$scope.changeStyle = function (){
+		if ($scope.stylePath == 'style.css') {
+			$scope.stylePath = 'style2.css';
+		}
+		else {
+			$scope.stylePath = 'style.css';
+		}
 	}
-
-	//call function to initially set board
-	$scope.setBoard();
-
-
-
-
-
-
-}
+};
